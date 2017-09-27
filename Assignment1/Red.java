69fe5f1e521dfb997567590048624fa892681397
dalek-who <839991169@qq.com>
2017-09-28 07:23:29 +0800

    进行了一些修改，使随机金额只有两位小数

--- Assignment1/Red.java
+++ Assignment1/Red.java
@@ -9,7 +9,7 @@ public class Red {
 		//输入钱数和人数
 		Scanner input = new Scanner(System.in);
 		System.out.println("请输入红包总价格（单位：元）: " );
-		float money = input.nextFloat();
+		double money = input.nextDouble();
 		
 		System.out.println("请输入人数: " );
 		int people = input.nextInt();
@@ -17,15 +17,15 @@ public class Red {
 		input.close();
 		
 		//一个人得到的钱
-		float ones_money;
+		double ones_money;
 		
 		//随机数
 		Random random_float = new Random(),random_int = new Random();
 		int i=0;
 		for(i=1 ; i<=people ; ++i) {
-			//生成每个人的随机钱数
+			//生成每个人的随机钱数 
 			do {
-				ones_money = random_int.nextInt((int)money+1) + random_float.nextFloat();
+				ones_money = random_int.nextInt((int)money+1) + random_float.nextInt(100) /100.0;
 			}while(ones_money < 0 || ones_money > money);
 			
 			System.out.println("第"+ i + "人：" + ones_money);
